b1ff39c3b3c3e70bf3eb311469207c56ae73f316
Mohd Ashif Ali <ali.mashifali.ashif14@gmail.com>
2019-08-17 19:44:19 +0530

    Refactoring

--- src/string/Permutation.java
+++ src/string/Permutation.java
@@ -6,8 +6,21 @@ package string;
 public class Permutation {
 
     public void print(String str) {
+        helper(str, "");
     }
 
+    private void helper(String str, String pre) {
+
+        if(str.length() == 0) {
+            System.out.println(pre);
+            return;
+        }
+
+        for(int i=0; i < str.length(); i++) {
+            String r = str.substring(0,i) + str.substring(i+1);
+            helper(r, pre + str.charAt(i));
+        }
+    }
     public static void main(String[] args) {
         String str = "abc";
         new Permutation().print(str);
