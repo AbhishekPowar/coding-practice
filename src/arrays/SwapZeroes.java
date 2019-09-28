58abc428666fdcd927fac6fe866baba2a7db58ce
Mohd Ashif Ali <ali.mashifali.ashif14@gmail.com>
2019-09-28 20:17:48 +0530

    commit

--- src/arrays/SwapZeroes.java
+++ src/arrays/SwapZeroes.java
@@ -18,10 +18,25 @@ public class SwapZeroes {
         for(int i=updateIdx; i<ar.length; i++)
             ar[i] = 0;
     }
-    public static void main(String[] args) {
-        int[] ar = {3,0,0, 7, 1, 2, 0};
 
-        new SwapZeroes().swaps(ar);
+    public void swap(int[] ar) {
+        int swapIdx = 0;
+        int currIdx = 0;
+
+        while(currIdx < ar.length) {
+            if(ar[currIdx] != 0) {
+                int tmp = ar[swapIdx];
+                ar[swapIdx] = ar[currIdx];
+                ar[currIdx] = tmp;
+                swapIdx++;
+            }
+            currIdx++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar = {3,0,0, 7, 1, 2, 0,4};
+
+        new SwapZeroes().swap(ar);
         for(int i=0; i<ar.length; i++)
             System.out.println(ar[i]);
 
