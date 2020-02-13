2aa373e1daab7bdfbbc4a8dd8e277f68d398841d
Mohd Ashif Ali <mohdasha.dev@gmai.com>
2020-02-13 22:19:27 +0530

    Commit

---
+++ src/java8/collectors/Example.java
@@ -0,0 +1,28 @@
+package java8.collectors;
+
+import java8.Employee;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Example {
+    public static void main(String[] args) {
+        List<Employee> employees;
+
+        Employee e1 = new Employee(1, "Ashif", 25);
+        Employee e2 = new Employee(2, "Swapnil", 28);
+        Employee e3 = new Employee(3, "Nikita", 25);
+
+        employees = Arrays.asList(e1, e2, e3);
+
+        Map<Integer, Long> e = employees.stream()
+                .filter(employee -> employee.getAge() < 28)
+                .collect(
+                        Collectors.groupingBy((r) -> r.getAge(), Collectors.counting())
+                );
+
+        System.out.println(e);
+    }
+}
